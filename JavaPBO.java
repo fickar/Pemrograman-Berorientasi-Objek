7b7ddb06cac38110e28eda0d17cbc4120cce9f00
Zulfikar Hikmatyar <89756828+flintcups@users.noreply.github.com>
2022-08-31 18:09:56 +0700

    Add files via upload

---
+++ JavaPBO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javapbo;
+
+/**
+ *
+ * @author MSI
+ */
+public class JavaPBO {
+            System.out.println("FFFFFFFFFF      I       K      K");
+            System.out.println("F               I       K     K");
+            System.out.println("F               I       K  K");
+            System.out.println("FFFFFFFFFF      I       K K");
+            System.out.println("F               I       K  K");
+            System.out.println("F               I       K    K");
+            System.out.println("F               I       K      K");
+    /**
+     * @param args the command line arguments
+     */
+        
+         
+                
+        
+        
+
+
+        // TODO code application logic here
+    }
+    
+}
